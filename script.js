6253768b5f578dfe0eca644c31af683b5303ae6a
nicksincl <hellonicksinclair@outlook.com>
2022-06-19 12:26:28 +0100

    added ability to edit color vars with color pickers

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('Hello!');
+let mainColor, secondaryColor, accentColor;
+
+window.addEventListener('load', startup, false);
+
+function startup() {
+  mainColor = document.querySelector('#main-color');
+  secondaryColor = document.querySelector('#secondary-color');
+  accentColor = document.querySelector('#accent-color');
+
+  mainColor.addEventListener('input', updateFirst, false);
+  mainColor.addEventListener('change', updateAll, false);
+  secondaryColor.addEventListener('input', updateFirst, false);
+  secondaryColor.addEventListener('change', updateAll, false);
+  accentColor.addEventListener('input', updateFirst, false);
+  accentColor.addEventListener('change', updateAll, false);
+
+  mainColor.select();
+  secondaryColor.select();
+  accentColor.select();
+}
+
+function updateFirst(event) {
+  document.documentElement.style.setProperty(
+    '--main-color',
+    event.target.value
+  );
+}
+
+function updateAll(event) {
+  document.documentElement.style.setProperty(
+    '--main-color',
+    event.target.value
+  );
+}
